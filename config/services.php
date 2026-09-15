0f65741681f3f93f1611e7ef2c6ff24ad4df8f1e
AbdallahSaeed0 <bodyahly17@gmail.com>
2026-09-16 01:13:58 +0300

    Update services.php

--- config/services.php
+++ config/services.php
@@ -60,7 +60,7 @@ return [
         'iap_shared_secret' => env('APPLE_IAP_SHARED_SECRET'),
         'iap_key_id' => env('APPLE_IAP_KEY_ID'),
         'iap_issuer_id' => env('APPLE_IAP_ISSUER_ID'),
-        'iap_private_key_path' => env('APPLE_IAP_PRIVATE_KEY_PATH', storage_path('app/apple/SubscriptionKey_UUGN979Z74.p8')),
+        'iap_private_key_path' => env('APPLE_IAP_PRIVATE_KEY_PATH', storage_path('app/apple/SubscriptionKey_' . env('APPLE_IAP_KEY_ID') . '.p8')),
         'iap_bundle_id' => env('APPLE_IAP_BUNDLE_ID', 'com.education.coursesApp'),
     ],
 
